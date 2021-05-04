fb2f9ef19ba063d6dd44cb1e278f637540e0bd83
sumaiyaazad <azadsumaiya00@gmail.com>
2021-05-04 12:10:54 +0600

    added api-gateway

--- currency-conversion-service/src/main/java/com/taaha/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/taaha/currencyconversionservice/CurrencyExchangeProxy.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="netflix-zuul-api-gateway-server")
 //@RibbonClient(name="currency-exchange-service")
-@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
